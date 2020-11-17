68aabd5a815754228578cb5f2bef56838e668b38
Benjamin Ward <benjaminloganward@gmail.com>
2020-11-17 15:58:06 -0500

    Refactor local image reading into separate file, add file size limit

--- localImageSource.go
+++ localImageSource.go
@@ -1,1 +1,55 @@
 package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type LocalImageSource struct {
+	fs http.FileSystem
+	maxSize int64
+}
+
+func (is LocalImageSource) Read(name string) ([]byte, error) {
+	// Open the file from the local filesystem for reading
+	file, err := is.fs.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { err = file.Close() }() // Make sure we actually close the file
+
+	// Read file information for later use
+	info, err := file.Stat()
+	if err != nil {  return nil, err }
+
+	// Throw a 404 if someone tries to read a directory. We also don't care about any index.html file serving
+	if info.IsDir() {
+		fmt.Printf("Trying to read from directory\n")
+		return nil, os.ErrNotExist
+	}
+
+	// Reject resizing any images that are too large
+	if info.Size() > is.maxSize {
+		// TODO: Maybe this should be a different error, since it /does/ exist, but we
+		//  probably don't want to leak state
+		fmt.Printf("Image was too large, not loading...")
+		return nil, os.ErrNotExist
+	}
+
+	// TODO: Before we read in the file, maybe we should check if it's actually an image.
+	//  Perhaps this could be accomplished with DetectContentType, reading in 512 bytes only.
+	// <https://stackoverflow.com/questions/25959386/how-to-check-if-a-file-is-a-valid-image>
+	// <https://socketloop.com/tutorials/golang-how-to-verify-uploaded-file-is-image-or-allowed-file-types>
+
+
+	// Decoder wants []byte, so read the whole file into a buffer
+	inputBuf, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Printf("Failed to read input file, %s\n", err)
+		return nil, err // TODO: We might not want to leak state here, maybe just send a 404
+	}
+
+	return inputBuf, nil
+}
--- main.go
+++ main.go
@@ -4,11 +4,13 @@ package main
 // https://news.ycombinator.com/item?id=15696855
 // For later reference: https://github.com/imgproxy/imgproxy
 
+// TODO: Consider rewriting this using fasthttp <https://github.com/valyala/fasthttp>
+// TODO: Look into how the http.FileServer safety guarantees work; we need them here
+
 import (
 	"flag"
 	"fmt"
 	"github.com/discordapp/lilliput"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -16,46 +18,33 @@ import (
 	_ "net/http/pprof"
 )
 
-type thumbnailFileSystem struct {
-	fs http.FileSystem
-	tp ThumbnailProcessor
-	// TODO: Add image source variable to hold remote vs local image source configuration
+type ImageSource interface {
+	Read(name string) ([]byte, error)
 }
 
-// TODO: Consider rewriting this using fasthttp https://github.com/valyala/fasthttp
-// TODO: Look into how the http.FileServer safety guarantees work; we need them here
+type thumbnailFileSystem struct {
+	tp ThumbnailProcessor
+	is ImageSource
+}
+
+
 func (fs thumbnailFileSystem) Open(name string) (http.File, error) {
 
-	// TODO: Move file opening/checking/reading code to localImageSource
-	file, err := fs.fs.Open(name)
+	// Read the image from the specified (remote or local) image source
+	imageBuffer, err := fs.is.Read(name)
 	if err != nil {
+		fmt.Printf("Failed to read image from specified source\n")
 		return nil, err
 	}
-	// Make sure we actually close the file
-	defer func() { err = file.Close() } ()
 
-	// TODO: Refactor this so that we can just reject resizing any images that are too large
-	// TODO: When this fetches images from a remote URL instead of from filesystem, we can do this based on image headers
-	// Throw a 404 if someone tries to read a directory. We also don't care about any index.html file serving
-	if info, err := file.Stat(); err == nil && info.IsDir() {
-		fmt.Printf("trying to read from directory\n")
-		return nil, os.ErrNotExist
-	}
-
-	// Decoder wants []byte, so read the whole file into a buffer
-	inputBuf, err := ioutil.ReadAll(file)
+	// Process the image into a thumbnail
+	thumbnailFile, err := fs.tp.process(imageBuffer, name)
 	if err != nil {
-		fmt.Printf("failed to read input file, %s\n", err)
+		fmt.Printf("Failed to profess thumbnail, %s\n", err)
 		return nil, err // TODO: We might not want to leak state here, maybe just send a 404
 	}
 
-	thumbnailFile, err := fs.tp.process(inputBuf, name)
-	if err != nil {
-		fmt.Printf("failed to profess thumbnail, %s\n", err)
-		return nil, err // TODO: We might not want to leak state here, maybe just send a 404
-	}
-
-	fmt.Printf("finishing file open\n")
+	fmt.Printf("Finishing file open\n")
 	return thumbnailFile, nil
 }
 
@@ -70,11 +59,13 @@ func main() {
 
 	var imageRoot string
 	var poolSize int
+	var maxImageSize int64
 
 	// Capture configuration parameters from CLI flags
 	// TODO: Add flags for remote URL and switch between local and remote sources
 	flag.StringVar(&imageRoot, "imageRoot", "", "absolute path to image storage")
 	flag.IntVar(&poolSize, "poolSize", 5, "number of image buffers to allocate")
+	flag.Int64Var(&maxImageSize, "maxImageSize", 20*1024*1024, "maximum image size to accept for resize, in bytes")
 	flag.Parse()
 
 	if imageRoot == "" {
@@ -83,14 +74,19 @@ func main() {
 		os.Exit(1)
 	}
 
+	dirFs := http.Dir(imageRoot)
 	thumbnailProcessor := NewThumbnailProcessor(encodeOptions, poolSize)
+	imageSource := LocalImageSource{
+		fs:      dirFs,
+		maxSize: maxImageSize,
+	}
 
 	// See Example(DotFileHiding): https://golang.org/pkg/net/http/#FileServer
 	// See Disable directory listing with http.FileServer: https://groups.google.com/u/2/g/golang-nuts/c/bStLPdIVM6w
 	// See Prevent Access to Files in Folder: https://stackoverflow.com/questions/40716869/prevent-access-to-files-in-folder-with-a-golang-server
-	fs := thumbnailFileSystem{http.Dir(imageRoot), thumbnailProcessor}
+	thumbnailFs := thumbnailFileSystem{thumbnailProcessor, imageSource}
 
 	// See Example(StripPrefix): https://golang.org/pkg/net/http/#FileServer
-	http.Handle("/thumb/", http.StripPrefix("/thumb/", http.FileServer(fs)))
+	http.Handle("/thumb/", http.StripPrefix("/thumb/", http.FileServer(thumbnailFs)))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
